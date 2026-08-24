3fa86bde51d62d6cf1f3dcbdb976cf128c0fdf72
LAPTOP-ILH15AP4\academia <tonchi.w.3@gmail.com>
2026-08-24 11:46:07 +0900

    ７－４まで

---
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.todoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "ToDo");
+        return "index";
+    }
+}
